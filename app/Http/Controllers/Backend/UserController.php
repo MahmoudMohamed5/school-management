bf8c734340376d427d08dcf7c815e24502f09bb4
Mhmod <mahmoud97787@gmail.com>
2026-09-22 15:05:04 +0300

    view user date from database

--- app/Http/Controllers/Backend/UserController.php
+++ app/Http/Controllers/Backend/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -12,7 +13,8 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('admin.user.view');
+        $data['users'] = User::all();
+        return view('admin.user.view', $data);
     }
 
     /**
--- resources/views/admin/user/view.blade.php
+++ resources/views/admin/user/view.blade.php
@@ -20,35 +20,37 @@
 				<!-- /.box-header -->
 				<div class="box-body">
 					<div class="table-responsive">
-					  <table id="example1" class="table table-bordered table-striped">
+					  <table id="example1" class="table table-bordered table-striped text-center align-middle">
 						<thead>
 							<tr>
+								<th>#</th>
+								<th>Role</th>
 								<th>Name</th>
-								<th>Position</th>
-								<th>Office</th>
-								<th>Age</th>
-								<th>Start date</th>
-								<th>Salary</th>
+								<th>Email</th>
+								<th>Edit</th>
+								<th>Delete</th>
 							</tr>
 						</thead>
 						<tbody>
+							@foreach ( $users as $key => $user )
 							<tr>
-								<td>Donna Snider</td>
-								<td>Customer Support</td>
-								<td>New York</td>
-								<td>27</td>
-								<td>2011/01/25</td>
-								<td>$112,000</td>
+								<td>{{ $key + 1 }}</td>
+								<td>{{ $user->user_type }}</td>
+								<td>{{ $user->name }}</td>
+								<td>{{ $user->email }}</td>
+								<td><a href="#" class="btn btn-info"><i class="fa fa-edit "></i> Edit</a></td>
+								<td><a href="#" class="btn btn-danger"><i class="fa fa-trash"></i> Delete</a></td>
 							</tr>
+							@endforeach
 						</tbody>
 						<tfoot>
 							<tr>
+								<th>#</th>
+								<th>Role</th>
 								<th>Name</th>
-								<th>Position</th>
-								<th>Office</th>
-								<th>Age</th>
-								<th>Start date</th>
-								<th>Salary</th>
+								<th>Email</th>
+								<th>Edit</th>
+								<th>Delete</th>
 							</tr>
 						</tfoot>
 					  </table>
